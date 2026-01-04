77b9ddfa756d0671d8af5d40d8d904b2e1c59b08
TamilNangaiK <k.tamilpirai@gmail.com>
2026-01-05 00:30:16 +0530

    initial

--- cafe/tailwind.config.js
+++ cafe/tailwind.config.js
@@ -5,17 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        marquee: 'marquee 120s linear infinite', 
-      },
-      keyframes: {
-        marquee: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-50%)' }, 
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
